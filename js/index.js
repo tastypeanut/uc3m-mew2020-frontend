2a38fe021fb4007f1cdc6cb3ee969258ae385ecf
tastypeanut <tastypeanut@users.noreply.github.com>
2020-11-21 16:55:30 +0100

    Añadidas cosas

--- js/index.js
+++ js/index.js
@@ -1,3 +1,19 @@
 if (getCookie('sessionID') == null) {
     window.location.href = "auth.html";
 }
+
+$(document).ready(
+    function(){
+        var GETrequestJSON = JSON.parse(httpGet("http://localhost:8000/events"));
+
+    }
+);
+
+
+function httpGet(Url)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", Url, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
